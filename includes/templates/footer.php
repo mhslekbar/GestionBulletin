abc76ae664989f00ab6254088bbc7ab91ee07e70
mhslekbar <mhs.lekbar@gmail.com>
2022-01-10 21:13:11 +0000

    Create footer.php

---
+++ includes/templates/footer.php
@@ -0,0 +1,9 @@
+
+
+    <script src="<?= $js . "jquery-3.6.0.min.js"; ?>"></script>
+    <script src="<?= $js . "bootstrap-popper.min.js"; ?>"></script>
+    <script src="<?= $js . "bootstrap.min.js"; ?>"></script>
+    <script src="<?= $js . "printThis.js"; ?>"></script>
+    <script src="<?= $js . "main.js"; ?>"></script>
+  </body>
+</html>
