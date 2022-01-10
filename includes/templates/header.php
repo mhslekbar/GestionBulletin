2b6e76adddc747dd01550e3ab8c07c57c283e2f3
mhslekbar <mhs.lekbar@gmail.com>
2022-01-10 21:13:40 +0000

    Create header.php

---
+++ includes/templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sniper</title>
+    <link href="<?= $css . "bootstrap.min.css";?>" rel="stylesheet">
+    <link href="<?= $css . "all.min.css";?>" rel="stylesheet">
+    <link href="<?= $css . "main.css";?>" rel="stylesheet">
+</head>
+<body>
+
+
+
