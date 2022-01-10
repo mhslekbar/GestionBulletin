64f21937d23d24cc34083351a0a1403322494af0
mhslekbar <mhs.lekbar@gmail.com>
2022-01-10 21:11:15 +0000

    Create checkMatricule.php

---
+++ noteCode/checkMatricule.php
@@ -0,0 +1,17 @@
+<?php
+    include  "../connect.php";
+    $matricule = $_GET['regNo'] ?? null;
+    
+    if(check($matricule) > 0){
+        header("content-type: application/json");
+        echo json_encode(check($matricule));
+    }
+
+    function check($matricule){
+        global $conn;
+        $stmt = $conn->prepare("SELECT * , n.idMatiere as Matiere , FullName FROM notes n JOIN matieres m JOIN  classes c JOIN etudiants e ON e.Matricule = n.Matricule AND m.idMatiere = n.idMatiere AND n.idClasse = c.idClasse WHERE n.Matricule = ? ");
+        $stmt->execute([$matricule]);
+        return $stmt->fetchAll();
+    }
+
+?>
